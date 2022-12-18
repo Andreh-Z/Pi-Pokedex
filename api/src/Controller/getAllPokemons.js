c1593b509fa2f48289a3f5dc276a877e42e48a07
Andreh-Z <witecan931@gmail.com>
2022-12-18 02:25:28 -0300

    controladores explicados

--- api/src/Controller/getAllPokemons.js
+++ api/src/Controller/getAllPokemons.js
@@ -1,18 +1,28 @@
 const { Type, Pokemon } = require("../db");
 const fetch = require("node-fetch");
 
+// Función que recibe como parámetros un límite de resultados y un nombre de Pokémon
+// Si el nombre de Pokémon es "null", devuelve una lista de objetos con la información de cada uno de los Pokémon que se encuentran en la API de Pokémon
+// Si el nombre de Pokémon no es "null", devuelve una lista con un único objeto con la información del Pokémon especificado
 const pokemonsApi = async (limit, name) => {
   if (!name) {
+    // Si no se especificó un nombre de Pokémon, se construye la URL de la API de Pokémon con el límite de resultados especificado
     let url = `https://pokeapi.co/api/v2/pokemon?limit=${limit}`;
+    // Se hace una petición a la API de Pokémon
     const api = await fetch(url);
+    // Se transforma la respuesta de la API a formato JSON
     const x = await api.json();
+    // Se obtiene una lista con las URLs de cada uno de los Pokémon que se encuentran en la respuesta de la API
     const arrayResults = x.results;
     const arrayUrls = arrayResults.map((result) => result.url);
+    // Se hacen peticiones a cada una de las URLs de los Pokémon
     const pokemonesApi = await Promise.all(arrayUrls.map((url) => fetch(url)));
+    // Se transforman las respuestas de las peticiones a formato JSON
     const arrayPokemonsApi = await Promise.all(
       pokemonesApi.map((response) => response.json())
     );
 
+    // Se devuelve una lista con objetos que contienen la información de cada uno de los Pokémon de la API
     return arrayPokemonsApi.map((e) => ({
       id: e.id,
       name: e.name,
@@ -26,9 +36,13 @@ const pokemonsApi = async (limit, name) => {
       peso: e.weight,
     }));
   } else {
+    // Si se especificó un nombre de Pokémon, se construye la URL de la API de Pokémon con el nombre del Pokémon
     let url = `https://pokeapi.co/api/v2/pokemon/${name}`;
+    // Se hace una petición a la API de Pokémon
     const api = await fetch(url);
+    // Se transforma la respuesta de la API a formato JSON
     const x = await api.json();
+    // Se devuelve una lista con un único objeto que contiene la información del Pokémon especificado
     return [
       {
         id: x.id,
@@ -46,8 +60,10 @@ const pokemonsApi = async (limit, name) => {
   }
 };
 
+// Función que obtiene la información de todos los Pokémon que se encuentran en la base de datos
 const pokeDbInfo = async () => {
   return await Pokemon.findAll({
+    // Se incluyen los tipos de cada Pokémon en la consulta a la base de datos
     include: {
       model: Type,
       attribute: ["name"],
@@ -58,16 +74,22 @@ const pokeDbInfo = async () => {
   });
 };
 
+// Función que recibe como parámetros un límite de resultados y un nombre de Pokémon
+// Devuelve una lista de objetos con la información de cada uno de los Pokémon que se encuentran en la API de Pokémon y en la base de datos
 const allPokemons = async (limit, name) => {
   let getPokemons;
   if (name) {
+    // Si se especificó un nombre de Pokémon, se obtiene la información del Pokémon especificado tanto de la API de Pokémon como de la base de datos
     getPokemons = (await pokemonsApi(limit, name))[0];
   } else {
+    // Si no se especificó un nombre de Pokémon, se obtiene la información de todos los Pokémon de la API de Pokémon y de la base de datos
     const pokeApi = await pokemonsApi(limit);
     const pokeDB = await pokeDbInfo();
+    // Se concatenan los resultados de la API de Pokémon y de la base de datos
     getPokemons = pokeApi.concat(pokeDB);
   }
   return getPokemons;
 };
 
+// Se exportan las funciones para poder ser utilizadas en otras partes del código
 module.exports = { allPokemons, pokeDbInfo, pokemonsApi };
--- api/src/Controller/pokemonId.js
+++ api/src/Controller/pokemonId.js
@@ -1,24 +1,32 @@
+// Importamos la dependencia 'node-fetch' para hacer solicitudes HTTP
 const fetch = require("node-fetch");
 
+// Creamos la función 'pokemonId' que recibe un ID de Pokemon como parámetro
 const pokemonId = async (id) => {
-  // Hacer una solicitud a la API de Pokemon para recuperar un solo Pokemon basado en su ID
+  // Construimos la URL para hacer la solicitud a la API de Pokemon
+  // utilizando el ID del Pokemon que se recibió como parámetro
   let url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+
+  // Hacemos una solicitud GET a la URL construida
   const apiResponse = await fetch(url);
+
+  // Parseamos la respuesta de la API como JSON
   const pokemonData = await apiResponse.json();
 
-  // Devuelve un objeto con los datos del Pokemon
+  // Devolvemos un objeto con los datos del Pokemon
   return {
-    id: pokemonData.id,
-    name: pokemonData.name,
-    image: pokemonData.sprites.front_default,
-    vida: pokemonData.stats[0].base_stat,
-    ataque: pokemonData.stats[1].base_stat,
-    defensa: pokemonData.stats[2].base_stat,
-    velocidad: pokemonData.stats[5].base_stat,
-    tipo: pokemonData.types.map((element) => element.type.name),
-    altura: pokemonData.height,
-    peso: pokemonData.weight,
+    id: pokemonData.id, // ID del Pokemon
+    name: pokemonData.name, // Nombre del Pokemon
+    image: pokemonData.sprites.front_default, // Imagen del Pokemon
+    vida: pokemonData.stats[0].base_stat, // PV del Pokemon
+    ataque: pokemonData.stats[1].base_stat, // Ataque del Pokemon
+    defensa: pokemonData.stats[2].base_stat, // Defensa del Pokemon
+    velocidad: pokemonData.stats[5].base_stat, // Velocidad del Pokemon
+    tipo: pokemonData.types.map((element) => element.type.name), // Tipo(s) del Pokemon
+    altura: pokemonData.height, // Altura del Pokemon
+    peso: pokemonData.weight, // Peso del Pokemon
   };
 };
 
+// Exportamos la función 'pokemonId' para que pueda ser utilizada en otros módulos
 module.exports = { pokemonId };
